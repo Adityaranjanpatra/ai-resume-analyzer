9e556685b060b2228bd6da28e1341c646c6e71e6
Adityaranjanpatra <aditya.dbiit2@gmail.com>
2025-07-25 18:52:09 +0530

    added favicon

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "InsightResume" },
+    { name: "description", content: "InsightResume is an AI-powered tool that analyzes resumes to extract key information and provide actionable feedback for improved hiring outcomes." },
   ];
 }
 
